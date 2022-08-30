2f43d511cae20d46a82fbb2a9da285044a62eb7c
EvilHero90 <Shadow280690@hotmail.de>
2022-08-30 02:05:13 +0200

    tile

--- src/luaposition.cpp
+++ src/luaposition.cpp
@@ -1,3 +1,6 @@
+// Copyright 2022 The Forgotten Server Authors. All rights reserved.
+// Use of this source code is governed by the GPL-2.0 License that can be found in the LICENSE file.
+
 #include "otpch.h"
 #include "luaposition.h"
 #include "luascript.h"
